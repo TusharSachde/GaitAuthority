57e7f31830dac13dc8e7af4038dbdad260d44bfe
Tushar <tushar@Macintosh-2.local>
2014-09-20 20:17:58 +0530

    Chale Chalo

---
+++ js/app.js
@@ -0,0 +1,37 @@
+angular.module('starter', ['ionic', 'starter.controllers', 'ngCordova'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+    .state('app', {
+      url: "/app",
+      abstract: true,
+      templateUrl: "templates/menu.html",
+      controller: 'AppCtrl'
+    })
+
+    .state('app.home', {
+      url: "/home",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/home.html",
+          controller: 'HomeCtrl'
+        }
+      }
+    });
+    
+  $urlRouterProvider.otherwise('/app/home');
+});
+
---
+++ js/controllers.js
@@ -0,0 +1,40 @@
+angular.module('starter.controllers', ['ngCordova'])
+
+.controller('AppCtrl', function ($scope, $ionicModal, $timeout) {
+    // Form data for the login modal
+    $scope.loginData = {};
+
+    // Create the login modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/login.html', {
+        scope: $scope
+    }).then(function (modal) {
+        $scope.modal = modal;
+    });
+
+    // Triggered in the login modal to close it
+    $scope.closeLogin = function () {
+        $scope.modal.hide();
+    };
+
+    // Open the login modal
+    $scope.login = function () {
+        $scope.modal.show();
+    };
+})
+
+.controller('HomeCtrl', function ($scope, $stateParams, $cordovaCapture) {
+    $scope.captureVideo = function () {
+        console.log("Sunny Leone");
+        var options = {
+            limit: 3,
+            duration: 15
+        };
+
+        $cordovaCapture.captureVideo(options).then(function (videoData) {
+            // Success! Video data is here
+        }, function (err) {
+            // An error occured. Show a message to the user
+        });
+    }
+    $scope.autoHeight = window.innerHeight-43;
+});
