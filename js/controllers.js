a191cfb3c51877b2ca4c5f9bee9458c4047384e1
Abhay Amin <pixoloproductions@gmail.com>
2014-10-06 20:01:40 +0530

    vidsuccerr

--- js/controllers.js
+++ js/controllers.js
@@ -9,17 +9,27 @@ angular.module('starter.controllers', ['ngCordova'])
     };
         
         $scope.captureVideo = function () {
+            
         var options = {
             limit: 1,
             duration: 15
         };
+            
+        var captureSuccess = function(mediaFiles) {
+            var path = mediaFiles.fullPath;
+            console.log(path);
+        };
+        var captureError = function(error) {
+            console.log("error code"+error);
+        };
 
-        $cordovaCapture.captureVideo(options).then(function (videoData) {
+        $cordovaCapture.captureVideo(captureSuccess, captureError, options);
+        /*    (options).then(function (videoData) {
             $scope.path = videoData.fullPath;
             console.log($scope.path);
         }, function (err) {
             // An error occured. Show a message to the user
-        });
+        });*/
     }
 })
 
