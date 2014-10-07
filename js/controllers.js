19e28cef2e580f442a75e7f37ce60ac97e5afcc3
Abhay Amin <pixoloproductions@gmail.com>
2014-10-07 23:49:36 +0530

    play

--- js/controllers.js
+++ js/controllers.js
@@ -1,21 +1,47 @@
 angular.module('starter.controllers', ['ngCordova'])
 
-.controller('AppCtrl', function ($scope, $ionicModal, $timeout) {
-})
+.controller('AppCtrl', function ($scope, $ionicModal, $timeout) {})
 
-.controller('HomeCtrl', function ($scope, $stateParams, $cordovaCapture) {
-    
-    $scope.captureVideo2 = function () {
-    };
+.controller('HomeCtrl', function ($scope, $sce, $stateParams, $cordovaCapture) {
+
+    $scope.captureVideo2 = function () {};
+
+    $scope.playpause = function () {
+        console.log("func");
+        var vid1 = document.getElementById("video1"); 
         
-        $scope.captureVideo = function () {
-            
-       var options = {
+            vid1.play();
+     
+    };
+
+    $scope.config = {
+        sources: [
+            {
+                src: $sce.trustAsResourceUrl("videogular.mp4"),
+                type: "video/mp4"
+            },
+            {
+                src: $sce.trustAsResourceUrl("videogular.webm"),
+                type: "video/webm"
+            },
+            {
+                src: $sce.trustAsResourceUrl("videogular.ogg"),
+                type: "video/ogg"
+            }
+    ],
+        theme: {
+            url: "styles/themes/default/videogular.css"
+        }
+    };
+
+    $scope.captureVideo = function () {
+
+        var options = {
             limit: 1,
             duration: 15
         };
-            
-      /*  var captureSuccess = function(mediaFiles) {
+
+        /*  var captureSuccess = function(mediaFiles) {
             var path = mediaFiles[0].fullPath;
             console.log(path);
         };
@@ -32,8 +58,7 @@ angular.module('starter.controllers', ['ngCordova'])
     }
 })
 
-.controller('LoginpageCtrl', function($scope, $stateParams, $cordovaCapture) {
-    $scope.autoHeight = window.innerHeight-86;
+.controller('LoginpageCtrl', function ($scope, $stateParams, $cordovaCapture) {
+    $scope.autoHeight = window.innerHeight - 86;
 })
-.controller('ExitCtrl', function($scope, $stateParams, $cordovaCapture) {
-});
+    .controller('ExitCtrl', function ($scope, $stateParams, $cordovaCapture) {});
